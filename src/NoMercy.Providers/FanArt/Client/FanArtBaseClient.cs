83ab44ece8027a2322faf594fabfacb51d3c4946
Stoney_Eagle <45034970+StoneyEagle@users.noreply.github.com>
2025-11-30 15:01:50 +0100

    Update API keys and versioning across multiple files

--- src/NoMercy.Providers/FanArt/Client/FanArtBaseClient.cs
+++ src/NoMercy.Providers/FanArt/Client/FanArtBaseClient.cs
@@ -24,7 +24,11 @@ public class FanArtBaseClient : IDisposable
         _client.DefaultRequestHeaders.Accept.Clear();
         _client.DefaultRequestHeaders.Accept.Add(new("application/json"));
         _client.DefaultRequestHeaders.Add("User-Agent", Config.UserAgent);
-        _client.DefaultRequestHeaders.Add("api-key", ApiInfo.FanArtKey);
+        _client.DefaultRequestHeaders.Add("api-key", ApiInfo.FanArtApiKey);
+        if (string.IsNullOrEmpty(ApiInfo.FanArtClientKey))
+        {
+            _client.DefaultRequestHeaders.Add("client-key", ApiInfo.FanArtClientKey);
+        }
     }
 
     protected FanArtBaseClient(Guid id)
@@ -36,7 +40,11 @@ public class FanArtBaseClient : IDisposable
         _client.DefaultRequestHeaders.Accept.Clear();
         _client.DefaultRequestHeaders.Accept.Add(new("application/json"));
         _client.DefaultRequestHeaders.Add("User-Agent", Config.UserAgent);
-        _client.DefaultRequestHeaders.Add("api-key", ApiInfo.FanArtKey);
+        _client.DefaultRequestHeaders.Add("api-key", ApiInfo.FanArtApiKey);
+        if (string.IsNullOrEmpty(ApiInfo.FanArtClientKey))
+        {
+            _client.DefaultRequestHeaders.Add("client-key", ApiInfo.FanArtClientKey);
+        }
         Id = id;
     }
 
